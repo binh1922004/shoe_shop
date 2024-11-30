91c2239a9723fcf547a7fdaa89f5306995326837
DKNguyen13 <nd2004lk13@gmail.com>
2024-12-01 02:06:48 +0700

    login

---
+++ src/main/java/hcmute/com/ShoeShop/services/UserService.java
@@ -0,0 +1,41 @@
+package hcmute.com.ShoeShop.services;
+
+import hcmute.com.ShoeShop.entity.Users;
+import hcmute.com.ShoeShop.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserService{
+    @Autowired
+    private UserRepository userRepository;
+
+    public long count() {
+        return userRepository.count();
+    }
+
+    public void delete(Users user) {
+        userRepository.delete(user);
+    }
+
+    public List<Users> findAll() {
+        return userRepository.findAll();
+    }
+
+    public Page<Users> findAll(Pageable pageable) {
+        return userRepository.findAll(pageable);
+    }
+
+    public void saveUser(Users user) {
+        userRepository.save(user);
+    }
+
+    public Users findUserByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+}
---
+++ src/main/java/hcmute/com/ShoeShop/utlis/Constant.java
@@ -0,0 +1,7 @@
+package hcmute.com.ShoeShop.utlis;
+
+public class Constant {
+    public static final String SESSION_USER = "user";
+    public static final String COOKIE_REMEMBER = "username";
+    public static final String DIR = "C:\\Users\\acer\\Downloads";
+}
