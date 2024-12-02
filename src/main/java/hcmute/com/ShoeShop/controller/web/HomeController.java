822eb7214533a1565051cc996af393e1681dbf29
DKNguyen13 <nd2004lk13@gmail.com>
2024-12-02 23:45:25 +0700

    update 1

--- src/main/java/hcmute/com/ShoeShop/controller/web/HomeController.java
+++ src/main/java/hcmute/com/ShoeShop/controller/web/HomeController.java
@@ -12,4 +12,45 @@ public class HomeController {
     {
         return "web/index";
     }
+    @GetMapping("/1")
+    public String home1()
+    {
+        return "user/shop-list";
+    }
+    @GetMapping("/2")
+    public String home2()
+    {
+        return "user/my-account";
+    }
+    @GetMapping("/3")
+    public String home3()
+    {
+        return "user/shop";
+    }
+
+    @GetMapping("/4")
+    public String home4()
+    {
+        return "user/contact";
+    }
+    @GetMapping("/5")
+    public String home5()
+    {
+        return "user/blog";
+    }
+    @GetMapping("/6")
+    public String home6()
+    {
+        return "user/blog-details";
+    }
+    @GetMapping("/7")
+    public String home7()
+    {
+        return "user/blog";
+    }
+    @GetMapping("/8")
+    public String home8()
+    {
+        return "user/wishlist";
+    }
 }
