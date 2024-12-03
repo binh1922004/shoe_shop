ca8357c3a5530917a25d0384cd75b7a293feb7dd
NhatNguyen1784 <nguyennhatnguyen1782004@gmail.com>
2024-12-03 11:12:30 +0700

    fix index, shop show all product

--- src/main/java/hcmute/com/ShoeShop/controller/user/UserHomeController.java
+++ src/main/java/hcmute/com/ShoeShop/controller/user/UserHomeController.java
@@ -1,26 +1,42 @@
 package hcmute.com.ShoeShop.controller.user;
 
+import hcmute.com.ShoeShop.entity.Product;
 import hcmute.com.ShoeShop.entity.Users;
+import hcmute.com.ShoeShop.services.ProductService;
 import hcmute.com.ShoeShop.utlis.Constant;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/user")
 public class UserHomeController {
 
+    @Autowired
+    private ProductService productService;
+
     @GetMapping("/shop")
-    public String userHome(HttpSession session, Model model) {
+    public String userHome(@RequestParam(defaultValue = "0") int page,
+                           @RequestParam(defaultValue = "6") int size,
+                           HttpSession session, Model model) {
         Users u = (Users) session.getAttribute(Constant.SESSION_USER);
         if(u!=null) {
             model.addAttribute("user", u);
-            return "redirect:/shop";
+            Page<Product> productPage = productService.getPaginatedProducts(PageRequest.of(page, size));
+            model.addAttribute("products", productPage.getContent());
+            model.addAttribute("currentPage", page);
+            model.addAttribute("totalPages", productPage.getTotalPages());
+            return "user/shop";
         }
         else {
             return "redirect:/login";
         }
     }
+
 }
