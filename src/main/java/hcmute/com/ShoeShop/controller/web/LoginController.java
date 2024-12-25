34406119350476f53cf1e86b36affad0019d78f8
binh1922004 <binh1920042018@gmail.com>
2024-12-25 16:24:59 +0700

    edit login controller

--- src/main/java/hcmute/com/ShoeShop/controller/web/LoginController.java
+++ src/main/java/hcmute/com/ShoeShop/controller/web/LoginController.java
@@ -24,19 +24,6 @@ public class LoginController {
 
     @GetMapping("/login")
     public String login(HttpServletRequest req, Model model) {
-        Cookie[] cookies = req.getCookies();
-        String username = null;
-        if (cookies != null) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals(Constant.COOKIE_REMEMBER)) {
-                    username = cookie.getValue();
-                    break;
-                }
-            }
-        }
-        if (username != null) {
-            model.addAttribute("username", username);
-        }
         return "/web/login";
     }
 
